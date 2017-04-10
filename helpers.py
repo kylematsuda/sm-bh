0adf1e5ccbe7857fde4a624f7d62ba9fc0ec609b
Kyle Matsuda <Kyle@Kyles-MacBook-Pro.local>
2017-04-09 20:26:09 -0600

    km cleaned up code a little

--- helpers.py
+++ helpers.py
@@ -4,7 +4,10 @@ import math
 # Simulation parameters:
 # d = Number cutoff, chi = Entanglement cutoff,
 # L = Number of sites
-d = 7; chi = 10; L = 10;
+d = 7; chi = 10; L = 10; delta = 0.01
+
+# Model parameters:
+J = 1; U = 1;
 
 # Class for handling the Lambda, Gamma, and Theta tensors
 class TensorGroup(object):
@@ -26,7 +29,7 @@ class TensorGroup(object):
 		theta = np.transpose(theta,(1,2,0,3))
 		return theta
 
-	def update(self, V, l):
+	def Update(self, V, l):
 		# Build the appropriate Theta tensor
 		Theta = self.Build_Theta(l)
 		# Apply the unitary matrix V
@@ -42,23 +45,23 @@ class TensorGroup(object):
 		self.Lambda[l, :] = B[0:chi] / np.linalg.norm(B[0:chi])
 
 		# Find the new Gammas
-		A = np.reshape(A[:,0:chi], (d, chi, chi))
-		print np.shape(A)
 
+		# Gamma_l:
+		# First, reshape A
+		A = np.reshape(A[:,0:chi], (d, chi, chi))
+		# Next, "divide off" Lambda[l-1]
+		# (see http://inside.mines.edu/~lcarr/theses/mishmash_thesis_2008.pdf,
+		#	equation 3.40)
 		Gamma_l_new = np.tensordot(np.diag(OneOver(self.Lambda[l-1,:])), A, axes=(1,1))
 		Gamma_l_new = np.transpose(Gamma_l_new, (1,0,2))
 		self.Gamma[l,:,:,:] = Gamma_l_new
-		print np.shape(Gamma_l_new)
 
+		# Gamma_(l+1):
+		# Do the same thing (note that C has already been transposed)
 		C = np.reshape(C[:,0:chi], (d, chi, chi))
-		print np.shape(C)
-
 		Gamma_lp1_new = np.tensordot(np.diag(OneOver(self.Lambda[l+1,:])), C, axes=(1,1))
 		Gamma_lp1_new = np.transpose(Gamma_lp1_new, (1,0,2))
 		self.Gamma[l+1,:,:,:] = Gamma_lp1_new
-		print np.shape(Gamma_lp1_new)
-
-
 
 # Helper functions for initialization:
 
@@ -69,7 +72,7 @@ class TensorGroup(object):
 # flag = 0: if number_onsite is an integer, initialize n = n_onsite Mott insulator
 # 		if not, initialize w/ probability (n_onsite - floor(n_onsite))
 # flag = 1: approximate coherent state with alpha = sqrt(n_onsite)
-def Initialize_States(L, n_max, n_onsite, flag=0):
+def Initialize_States(L, n_max, n_onsite, flag):
 	# Initialize a matrix of zeros
 	mat = np.zeros((L, n_max))
 
@@ -104,7 +107,6 @@ def Initialize_States(L, n_max, n_onsite, flag=0):
 			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (np.sqrt(math.factorial(i)) * norm)
 	return mat
 
-
 # Initialize the coefficient tensors
 # See http://inside.mines.edu/~lcarr/theses/mishmash_thesis_2008.pdf,
 # Appendix A, assuming product wavefunctions
@@ -126,4 +128,18 @@ def OneOver(array):
 		if array[i] != 0:
 			array[i] = 1/complex(array[i])
 	return array
+
+
+# Build a, a_dag, n, n_2site
+a = np.zeros((d,d))
+for i in range(0,d-1):
+	a[i, i+1] = math.sqrt(i+1)
+a_dag = np.transpose(a)
+n_op = np.dot(a_dag, a)
+n_2site = np.kron(n_op, np.identity(d))
+# build two site Hamiltonian:
+H_2site = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.dot(n_2site, n_2site - np.identity(d**2))
+# Diagonalize 
+w,v = np.linalg.eig(H_2site)
+V_2site = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w)))), np.transpose(v)), (d,d,d,d))
 
--- tebd.py
+++ tebd.py
@@ -2,27 +2,19 @@ import numpy as np
 import math
 import helpers
 
-# number cutoff, entanglement cutoff
-d = helpers.d; chi = helpers.chi; L = helpers.L;
-# parameters
-J = 1; U = 1; delta = 0.01;
+# Init simulation and model parameters
+d = helpers.d; chi = helpers.chi; L = helpers.L; delta = helpers.delta
+J = helpers.J; U = helpers.U;
+# Choose your initial state:
+occupation = 1.3; state_flag = 0;
 
-init = helpers.Initialize_States(L, d, 1.2, flag=1)
-thing = helpers.TensorGroup(init)
+# Initialize the state, and the various tensors we need:
+init = helpers.Initialize_States(L, d, occupation, state_flag)
+sim = helpers.TensorGroup(init)
 
-# build a, a_dag, n, n_2site
-a = np.zeros((d,d))
-for i in range(0,d-1):
-	a[i, i+1] = math.sqrt(i+1)
-a_dag = np.transpose(a)
-n = np.dot(a_dag, a)
-n_2site = np.kron(n, np.identity(d))
-# build two site Hamiltonian:
-H_2site = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.dot(n_2site, n_2site - np.identity(d**2))
-# Diagonalize 
-w,v = np.linalg.eig(H_2site)
-U = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w)))), np.transpose(v)), (d,d,d,d))
-
-thing.update(U, 1)
+# Unitary two-site operator
+V = helpers.V_2site
 
 
+sim.Update(V, 0)
+print sim.Gamma
