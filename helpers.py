km probably fixed issues with crazy numerical error. changed definition of error to agree with mishmash thesis. seems to make sense now

helpers.py
@@ -1,14 +1,17 @@
 import numpy as np
 import math
 
+# Model parameters:
+J = 1; U = 1.5 * J;
+
+# # Cutoff for small numbers
+# cutoff = 1E-10
+
 # Simulation parameters:
 # d = Number cutoff, chi = Entanglement cutoff,
 # L = Number of sites
 # Note: require chi > d!!!
-d = 3; chi = 10; L = 5; delta = 0.01; N = 2;
-
-# Model parameters:
-J = 1; U = 1.5;
+d = 5; chi = 50; L = 10; delta = 0.01 / J; N = 100;
 
 # Class for handling the Lambda, Gamma, and Theta tensors
 class TensorGroup(object):
@@ -23,27 +26,49 @@ class TensorGroup(object):
 		Gamma_lp1 = self.Gamma[l+1]
 		Gamma_l = self.Gamma[l]
 
+		# if (l != 0 and l != L-2):
+		# 	theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:,:], axes=(1,1))
+		# 	theta = np.tensordot(Gamma_l[:,:,:], theta, axes=(2,0))
+		# 	theta = np.tensordot(np.diag(Lambda[l-1,:]), theta, axes=(1,1))
+		# 	theta = np.tensordot(theta, np.diag(Lambda[l+1,:]), axes=(-1, 0))
+
+		# 	# Now indices are [a_(l-1), i_l, i_(l+1), a_(l+1)]
+		# 	# Swap them to [i_l, i_(l+1), a_(l-1), a_(l+1)] to avoid confusion
+		# 	theta = np.transpose(theta,(1,2,0,3))
+	
+		# # Structure of Gamma^(0) and Gamma^(L-1) are different...
+		# elif (l == 0):
+		# 	theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:,:], axes=(1,1))
+		# 	theta = np.tensordot(Gamma_l[:,:], theta, axes=(1,0))
+		# 	theta = np.tensordot(theta, np.diag(Lambda[l+1,:]), axes=(-1, 0))
+		# elif (l == L-2):
+		# 	theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:], axes=(1,1))
+		# 	theta = np.tensordot(Gamma_l[:,:,:], theta, axes=(-1,0))
+		# 	theta = np.tensordot(np.diag(Lambda[l-1,:]), theta, axes=(1,1))
+		# 	# Swap indices to be (i_l, i_l + 1, a_(l-1))
+		# 	theta = np.transpose(theta, (1,2,0))
+		# return theta
+
+
+		# Trying alternate def of Theta to reduce numerical errors
 		if (l != 0 and l != L-2):
 			theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:,:], axes=(1,1))
 			theta = np.tensordot(Gamma_l[:,:,:], theta, axes=(2,0))
-			theta = np.tensordot(np.diag(Lambda[l-1,:]), theta, axes=(1,1))
-			theta = np.tensordot(theta, np.diag(Lambda[l+1,:]), axes=(-1, 0))
 
-			# Now indices are [a_(l-1), i_l, i_(l+1), a_(l+1)]
-			# Swap them to [i_l, i_(l+1), a_(l-1), a_(l+1)] to avoid confusion
-			theta = np.transpose(theta,(1,2,0,3))
+			# # Now indices are [i_l, a_(l-1), i_(l+1), a_(l+1)]
+			# # Swap them to [i_l, i_(l+1), a_(l-1), a_(l+1)] to avoid confusion
+			theta = np.transpose(theta, (0,2,1,3))
 
 		# Structure of Gamma^(0) and Gamma^(L-1) are different...
 		elif (l == 0):
 			theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:,:], axes=(1,1))
 			theta = np.tensordot(Gamma_l[:,:], theta, axes=(1,0))
-			theta = np.tensordot(theta, np.diag(Lambda[l+1,:]), axes=(-1, 0))
+			# Indices already [i_l, i_(l + 1), a_(l + 1)]
 		elif (l == L-2):
 			theta = np.tensordot(np.diag(Lambda[l,:]), Gamma_lp1[:,:], axes=(1,1))
-			theta = np.tensordot(Gamma_l[:,:,:], theta, axes=(2,0))
-			theta = np.tensordot(np.diag(Lambda[l-1,:]), theta, axes=(1,1))
-			# Swap indices to be (i_l, i_l + 1, a_(l-1))
-			theta = np.transpose(theta, (1,2,0))	
+			theta = np.tensordot(Gamma_l[:,:,:], theta, axes=(-1,0))
+			# Swap indices to be [i_l, i_(l + 1), a_(l - 1)]
+			theta = np.transpose(theta, (0,2,1))
 		return theta
 
 	# This follows the discussion in
@@ -65,32 +90,38 @@ class TensorGroup(object):
 			Theta = np.reshape(np.transpose(Theta, (0,2,1,3)), (d*chi, d*chi))
 			# A and transpose.C contain the new Gamma[l] and Gamma[l+1]
 			# B contains new Lambda[l]
-			A, B, C = np.linalg.svd(Theta); C = C.T
+			A, B, C = np.linalg.svd(Theta)
+			C = np.transpose(C)
 
 			# Truncate at chi eigenvalues and enforce normalization
-			# self.Lambda[l, :] = B[0:chi] / np.linalg.norm(B[0:chi])
-			self.Lambda[l, :] = B[0:chi]
+			self.Lambda[l, :] = B[0:chi] / np.linalg.norm(B[0:chi])
 
 			# Keep track of the truncation error accumulated on this step
-			self.tau += 1 - np.linalg.norm(B[0:chi])**2
+			self.tau += delta * (1 - np.linalg.norm(B[0:chi])**2)
 
 			# Find the new Gammas
 
 			# Gamma_l:
 			# First, reshape A
-			A = np.reshape(A[:,0:chi], (d, chi, chi))
+			A = np.reshape(A[:, 0:chi], (d, chi, chi))
 			# Next, "divide off" Lambda[l-1]
 			# (see http://inside.mines.edu/~lcarr/theses/mishmash_thesis_2008.pdf,
 			#	equation 3.40)
-			Gamma_l_new = np.tensordot(np.diag(OneOver(self.Lambda[l-1,:])), A, axes=(1,1))
-			Gamma_l_new = np.transpose(Gamma_l_new, (1,0,2))
+			
+			# Trying alternative def of Theta:
+			# Gamma_l_new = np.tensordot(np.diag(OneOver(self.Lambda[l-1,:])), A, axes=(1,1))
+			# Gamma_l_new = np.transpose(Gamma_l_new, (1,0,2))
+			Gamma_l_new = A
 			self.Gamma[l] = Gamma_l_new
 
 			# Gamma_(l+1):
 			# Do the same thing (note that C has already been transposed)
-			C = np.reshape(C[:,0:chi], (d, chi, chi))
-			Gamma_lp1_new = np.tensordot(np.diag(OneOver(self.Lambda[l+1,:])), C, axes=(1,1))
-			Gamma_lp1_new = np.transpose(Gamma_lp1_new, (1,0,2))
+			C = np.reshape(C[:, 0:chi], (d, chi, chi))
+			
+			# Trying alternative def of Theta
+			# Gamma_lp1_new = np.tensordot(np.diag(OneOver(self.Lambda[l+1,:])), C, axes=(1,1))
+			# Gamma_lp1_new = np.transpose(Gamma_lp1_new, (1,0,2))
+			Gamma_lp1_new = C
 			self.Gamma[l+1] = Gamma_lp1_new
 
 		# The Gamma_0 tensor has one less index... need to treat slightly differently
@@ -99,16 +130,16 @@ class TensorGroup(object):
 			Theta = np.reshape(Theta, (d, d*chi))
 			# A and transpose.C contain the new Gamma[l] and Gamma[l+1]
 			# B contains new Lambda[l]
-			A, B, C = np.linalg.svd(Theta); C = C.T
+			A, B, C = np.linalg.svd(Theta)
+			C = np.transpose(C)
 
 			# Enforce normalization
 			# Don't need to truncate here because chi is bounded by
-			# the dimension of the smaller subsystem, here = d < chi
-			# self.Lambda[l,0:d] = B / np.linalg.norm(B)
-			self.Lambda[l,0:d] = B
+			# the dimension of the smaller subsystem, here equals d < chi
+			self.Lambda[l,0:d] = B / np.linalg.norm(B)
 
 			# Keep track of the truncation error accumulated on this step
-			self.tau += 1 - np.linalg.norm(B)**2
+			self.tau += delta * (1 - np.linalg.norm(B)**2)
 
 			# Find the new Gammas
 			# Gamma_l:
@@ -117,37 +148,41 @@ class TensorGroup(object):
 			# Gamma_(l+1):
 			# Treat the l = 1 case normally...
 			C = np.reshape(C[:,0:chi], (d, chi, chi))
-			Gamma_lp1_new = np.tensordot(np.diag(OneOver(self.Lambda[l+1,:])), C, axes=(1,1))
-			Gamma_lp1_new = np.transpose(Gamma_lp1_new, (1,0,2))
+			# Gamma_lp1_new = np.tensordot(np.diag(OneOver(self.Lambda[l+1,:])), C, axes=(1,1))
+			# Gamma_lp1_new = np.transpose(Gamma_lp1_new, (1,0,2))
+			Gamma_lp1_new = C
 			self.Gamma[l+1] = Gamma_lp1_new
 
 		elif (l == L - 2):
 			# Reshape to a square matrix and do singular value decomposition:
-			Theta = np.reshape(Theta, (d*chi, d))
+			Theta = np.reshape(np.transpose(Theta, (0,2,1)), (d*chi, d))
 			# A and transpose.C contain the new Gamma[l] and Gamma[l+1]
 			# B contains new Lambda[l]
-			A, B, C = np.linalg.svd(Theta); C = C.T
+			A, B, C = np.linalg.svd(Theta)
+			C = np.transpose(C)
 
 			# Enforce normalization
 			# Don't need to truncate here because chi is bounded by
-			# the dimension of the smaller subsystem, here = d < chi
-			# self.Lambda[l,0:d] = B / np.linalg.norm(B)
-			self.Lambda[l,0:d] = B
+			# the dimension of the smaller subsystem, here equals d < chi
+			self.Lambda[l,0:d] = B / np.linalg.norm(B)
 
 			# Keep track of the truncation error accumulated on this step
-			self.tau += 1 - np.linalg.norm(B)**2
+			self.tau += delta * (1 - np.linalg.norm(B)**2)
 
 			# Find the new Gammas
 
 			# Treat the L-2 case normally:
 			A = np.reshape(A[:,0:chi], (d, chi, chi))
-			Gamma_l_new = np.tensordot(np.diag(OneOver(self.Lambda[l-1,:])), A, axes=(1,1))
-			Gamma_l_new = np.transpose(Gamma_l_new, (1,0,2))
+			# Gamma_l_new = np.tensordot(np.diag(OneOver(self.Lambda[l-1,:])), A, axes=(1,1))
+			# Gamma_l_new = np.transpose(Gamma_l_new, (1,0,2))
+			Gamma_l_new = A
 			self.Gamma[l] = Gamma_l_new
 
 			# Gamma_(L-1):
 			self.Gamma[l+1][:,0:d] = C
-		print (l, self.tau)
+		# print (l, self.tau)
+		# print B
+		# print (l, np.shape(A), np.shape(C))
 
 
 # Helper functions for initialization:
@@ -203,12 +238,6 @@ def lambda0():
 	mat[:,0] = 1
 	return mat
 
-# def Gamma0(coeffs):
-# 	mat = np.zeros((L, d, chi, chi), dtype=np.complex64)
-# 	for i in range(0, L):
-# 		mat[i, :, 0, 0] = coeffs[i,:]
-# 	return mat
-
 def Gamma0(coeffs):
 	Gamma = []
 
@@ -226,12 +255,14 @@ def Gamma0(coeffs):
 	Gamma.append(mat)
 	return Gamma
 
-# Divide a 1d array
-def OneOver(array):
-	for i in range(0, len(array)):
-		if array[i] != 0:
-			array[i] = 1/complex(array[i])
-	return array
+# # Divide a 1d array
+# def OneOver(array):
+# 	for i in range(0, len(array)):
+# 		if np.absolute(array[i]) >= cutoff:
+# 			array[i] = 1/complex(array[i])
+# 		else:
+# 			array[i] = 0
+# 	return array
 
 
 # Operator definitions:
@@ -242,12 +273,18 @@ for i in range(0,d-1):
 	a[i, i+1] = math.sqrt(i+1)
 a_dag = np.transpose(a)
 n_op = np.dot(a_dag, a)
-n_2site = np.kron(n_op, np.identity(d))
-# build two site Hamiltonian:
-H_2site = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.dot(n_2site, n_2site - np.identity(d**2))
+# Build two site Hamiltonian:
+H_2site = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.kron(np.dot(n_op, n_op - np.identity(d)), np.identity(d))
+# Need to treat the last link differently...
+# Need to include onsite term for the last site also
+H_2site_last = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.kron(np.dot(n_op, n_op - np.identity(d)), np.identity(d)) + (U/2) * np.kron(np.identity(d), np.dot(n_op, n_op - np.identity(d)))
 # Diagonalize 
 w,v = np.linalg.eig(H_2site)
-V_odd = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w) / 2))), np.transpose(v)), (d,d,d,d))
-V_odd_sq = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w)))), np.transpose(v)), (d,d,d,d))
-V_even = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w)))), np.transpose(v)), (d,d,d,d))
+V_odd = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-1j*delta*(w) / 2))), np.transpose(v)), (d,d,d,d))
+V_even = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-1j*delta*(w)))), np.transpose(v)), (d,d,d,d))
+# Same thing for last site
+wp, vp = np.linalg.eig(H_2site_last)
+V_odd_last = np.reshape(np.dot(np.dot(vp,np.diag(np.exp(-1j*delta*(wp) / 2))), np.transpose(vp)), (d,d,d,d))
+V_even_last = np.reshape(np.dot(np.dot(vp,np.diag(np.exp(-1j*delta*(wp)))), np.transpose(vp)), (d,d,d,d))
+
 