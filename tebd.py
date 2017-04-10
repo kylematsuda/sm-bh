cb85b5a18cf540e374a5987bbc571564e6696bf3
Kyle Matsuda <Kyle@Kyles-MacBook-Pro.local>
2017-04-09 19:00:04 -0600

    km starting to construct and update theta tensor

--- tebd.py
+++ tebd.py
@@ -6,7 +6,7 @@ d = 7; chi = 10; L = 10;
 # parameters
 J = 1; U = 1; delta = 0.01;
 
-# Initialze state vectors (product state)
+# Initialize state vectors (product state)
 # L sites, number cutoff nmax
 # n_onsite is the number on each site
 # flag: 0 is Fock state, 1 is (truncated) coherent state with mean occupation = n_onsite
@@ -42,10 +42,10 @@ def initialize_states(L, n_max, n_onsite, flag=0):
 		norm = 0
 		for i in range(0, n_max):
 			norm += math.pow(n_onsite, i) / math.factorial(i)
-		norm = math.sqrt(norm)
+		norm = np.sqrt(norm)
 		# Build coherent states
 		for i in range(0, n_max):
-			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (math.sqrt(math.factorial(i)) * norm)
+			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (np.sqrt(math.factorial(i)) * norm)
 	return mat
 
 # Initialize the coefficient tensors
@@ -62,10 +62,33 @@ def Gamma0(coeffs):
 		mat[i, :, 0, 0] = coeffs[i,:]
 	return mat
 
+# Need to do the shit with np.diag to retain the correct number of free indices!
+def build_theta(Lambda, Gamma, l):
+	# Build the Theta tensor
+	theta = np.tensordot(np.diag(Lambda[l,:]), Gamma[l+1,:,:,:], axes=(1,1))
+	theta = np.tensordot(Gamma[l,:,:,:], theta, axes=(2,0))
+	theta = np.tensordot(np.diag(Lambda[l-1,:]), theta, axes=(1,1))
+	theta = np.tensordot(theta, np.diag(Lambda[l+1,:]), axes=(-1, 0))
+
+	# Now indices are [a_(l-1), i_l, i_(l+1), a_(l+1)]
+	# Swap them to [i_l, i_(l+1), a_(l-1), a_(l+1)] to avoid confusion
+	theta = np.transpose(theta,(1,2,0,3))
+	print np.shape(theta)
+	return theta
+
+def update_theta(Theta, V, Lambda, Gamma, l):
+	Theta = np.tensordot(V, Theta, axes=([2,3], [0,1]))
+	Theta = np.reshape(np.transpose(Theta, (0,2,1,3)), (d*chi, d*chi))
+	A, B, C = np.linalg.svd(Theta)
+
+	# Truncate at chi eigenvalues and enforce normalization
+	Lambda[l, :] = B[0:chi] / np.sqrt(sum(B[0:chi]**2))
+	
+	
+
+
 init = initialize_states(L, d, 1.2, flag=1)
-print init
-print lambda0()
-print Gamma0(init)
+theta = build_theta(lambda0(), Gamma0(init), 1)
 
 
 # build a, a_dag, n, n_2site
@@ -80,7 +103,6 @@ H_2site = -J * (np.kron(a_dag, a) + np.kron(a, a_dag)) + (U / 2) * np.dot(n_2sit
 # Diagonalize 
 w,v = np.linalg.eig(H_2site)
 U = np.reshape(np.dot(np.dot(v,np.diag(np.exp(-delta*(w)))), np.transpose(v)), (d,d,d,d))
-
 
 
 
