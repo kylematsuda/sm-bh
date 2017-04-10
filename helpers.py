e61dbe95f22d23c59896f96e707e7fc9bc8878e2
Kyle Matsuda <Kyle@Kyles-MacBook-Pro.local>
2017-04-09 19:05:04 -0600

    km helpers file

---
+++ helpers.py
@@ -0,0 +1,48 @@
+# Class for handling the Theta tensor
+class ThetaTensor(object):
+
+	def __init__(self):
+		self.data = []
+
+
+# Initialize state vectors (product state)
+# L sites, number cutoff nmax
+# n_onsite is the number on each site
+# flag: 0 is Fock state, 1 is (truncated) coherent state with mean occupation = n_onsite
+# flag = 0: if number_onsite is an integer, initialize n = n_onsite Mott insulator
+# 		if not, initialize w/ probability (n_onsite - floor(n_onsite))
+# flag = 1: approximate coherent state with alpha = sqrt(n_onsite)
+def initialize_states(L, n_max, n_onsite, flag=0):
+	# Initialize a matrix of zeros
+	mat = np.zeros((L, n_max))
+
+	# If not coherent states
+	if (flag == 0):
+		if (math.floor(n_onsite) >= 0):
+			# Check that you're under the number cutoff
+			if (math.floor(n_onsite) < n_max):
+				mat[:,math.floor(n_onsite)] = 1
+			# If not, initialize to vacuum
+			else:
+				return mat
+
+			# Check for fractional occupation
+			if (n_onsite != math.floor(n_onsite)):
+				filling = n_onsite - math.floor(n_onsite)
+				rands = np.random.rand(L)
+				# Populate next highest state with probability = filling
+				for i in range(0, L):
+					if rands[i] < filling:
+						mat[i,math.floor(n_onsite)+1] = 1
+						mat[i,math.floor(n_onsite)] = 0
+	# If coherent state
+	elif (flag == 1):
+		# Calculate normalization factor
+		norm = 0
+		for i in range(0, n_max):
+			norm += math.pow(n_onsite, i) / math.factorial(i)
+		norm = np.sqrt(norm)
+		# Build coherent states
+		for i in range(0, n_max):
+			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (np.sqrt(math.factorial(i)) * norm)
+	return mat
--- tebd.py
+++ tebd.py
@@ -1,52 +1,53 @@
 import numpy as np
 import math
+import helpers
 
 # number cutoff, entanglement cutoff
 d = 7; chi = 10; L = 10;
 # parameters
 J = 1; U = 1; delta = 0.01;
 
-# Initialize state vectors (product state)
-# L sites, number cutoff nmax
-# n_onsite is the number on each site
-# flag: 0 is Fock state, 1 is (truncated) coherent state with mean occupation = n_onsite
-# flag = 0: if number_onsite is an integer, initialize n = n_onsite Mott insulator
-# 		if not, initialize w/ probability (n_onsite - floor(n_onsite))
-# flag = 1: approximate coherent state with alpha = sqrt(n_onsite)
-def initialize_states(L, n_max, n_onsite, flag=0):
-	# Initialize a matrix of zeros
-	mat = np.zeros((L, n_max))
+# # Initialize state vectors (product state)
+# # L sites, number cutoff nmax
+# # n_onsite is the number on each site
+# # flag: 0 is Fock state, 1 is (truncated) coherent state with mean occupation = n_onsite
+# # flag = 0: if number_onsite is an integer, initialize n = n_onsite Mott insulator
+# # 		if not, initialize w/ probability (n_onsite - floor(n_onsite))
+# # flag = 1: approximate coherent state with alpha = sqrt(n_onsite)
+# def initialize_states(L, n_max, n_onsite, flag=0):
+# 	# Initialize a matrix of zeros
+# 	mat = np.zeros((L, n_max))
 
-	# If not coherent states
-	if (flag == 0):
-		if (math.floor(n_onsite) >= 0):
-			# Check that you're under the number cutoff
-			if (math.floor(n_onsite) < n_max):
-				mat[:,math.floor(n_onsite)] = 1
-			# If not, initialize to vacuum
-			else:
-				return mat
+# 	# If not coherent states
+# 	if (flag == 0):
+# 		if (math.floor(n_onsite) >= 0):
+# 			# Check that you're under the number cutoff
+# 			if (math.floor(n_onsite) < n_max):
+# 				mat[:,math.floor(n_onsite)] = 1
+# 			# If not, initialize to vacuum
+# 			else:
+# 				return mat
 
-			# Check for fractional occupation
-			if (n_onsite != math.floor(n_onsite)):
-				filling = n_onsite - math.floor(n_onsite)
-				rands = np.random.rand(L)
-				# Populate next highest state with probability = filling
-				for i in range(0, L):
-					if rands[i] < filling:
-						mat[i,math.floor(n_onsite)+1] = 1
-						mat[i,math.floor(n_onsite)] = 0
-	# If coherent state
-	elif (flag == 1):
-		# Calculate normalization factor
-		norm = 0
-		for i in range(0, n_max):
-			norm += math.pow(n_onsite, i) / math.factorial(i)
-		norm = np.sqrt(norm)
-		# Build coherent states
-		for i in range(0, n_max):
-			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (np.sqrt(math.factorial(i)) * norm)
-	return mat
+# 			# Check for fractional occupation
+# 			if (n_onsite != math.floor(n_onsite)):
+# 				filling = n_onsite - math.floor(n_onsite)
+# 				rands = np.random.rand(L)
+# 				# Populate next highest state with probability = filling
+# 				for i in range(0, L):
+# 					if rands[i] < filling:
+# 						mat[i,math.floor(n_onsite)+1] = 1
+# 						mat[i,math.floor(n_onsite)] = 0
+# 	# If coherent state
+# 	elif (flag == 1):
+# 		# Calculate normalization factor
+# 		norm = 0
+# 		for i in range(0, n_max):
+# 			norm += math.pow(n_onsite, i) / math.factorial(i)
+# 		norm = np.sqrt(norm)
+# 		# Build coherent states
+# 		for i in range(0, n_max):
+# 			mat[:,i] = math.pow(n_onsite, float(i) / 2) / (np.sqrt(math.factorial(i)) * norm)
+# 	return mat
 
 # Initialize the coefficient tensors
 # See http://inside.mines.edu/~lcarr/theses/mishmash_thesis_2008.pdf,
@@ -83,8 +84,8 @@ def update_theta(Theta, V, Lambda, Gamma, l):
 
 	# Truncate at chi eigenvalues and enforce normalization
 	Lambda[l, :] = B[0:chi] / np.sqrt(sum(B[0:chi]**2))
-	
-	
+
+
 
 
 init = initialize_states(L, d, 1.2, flag=1)
